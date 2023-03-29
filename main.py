2cbaa3dd609d061ca64293cb4bb2f410794e4b1c
ali farvardin <mohammadali@duck.com>
2023-03-29 22:45:47 +0330

    price problem solve

--- main.py
+++ main.py
@@ -3,11 +3,22 @@ import pandas as pd
 from scraper import Digikala
 
 def df_creation():
-    dk=digikala(input("please enter ur search Item: "))
+    dk=Digikala(input("please enter ur search Item: "))
     names=dk.names()
     prices=dk.prices()
     statuses=dk.statuses()
     urls=dk.urls()
     total_scores=dk.total_scores()
     count_of_scores=dk.count_of_scores()
-    
+
+    digikala_list=dict({"نام محصول":names,
+                        "قیمت" : prices,
+                        "قابل معامله":statuses,
+                        "امتیاز کل":total_scores,
+                        "تعداد رای دهندگان":count_of_scores,
+                        "آدرس محصول" : urls,
+    })
+    return pd.DataFrame(digikala_list)
+
+
+df_creation().to_excel("digikala.xlsx")
--- scraper.py
+++ scraper.py
@@ -1,14 +1,97 @@
+# from requests import get
+
+# class Digikala:
+#     def __init__(self, search_term:str):
+#         """
+#         Initializes a Digikala search object.
+#         :param search_term: The term to search in Digikala.
+#         """
+#         self.serch_term = search_term
+#         self.total_page_url = f"https://api.digikala.com/v1/search/?q={self.serch_term}&page=1"
+#         self.global_url=f"https://api.digikala.com/v1/search/?q={self.serch_term}"
+
+#     def pager(self) -> int:
+#         """
+#         Gets the total number of pages in the search results.
+#         :return: The total number of pages in the search results.
+#         """
+#         total_pages = get(url=self.total_page_url).json()['data']['pager'].get("total_pages")
+#         return int(total_pages)
+
+#     def names(self) -> tuple:
+#         """
+#         Gets the names of the products from the search results.
+#         :return: A tuple containing the names of the products.
+#         """
+#         names_ = (product.get("title_fa") for page in range(1, self.pager() + 1)
+#                   for product in get(self.global_url+f"&page={page}").json()['data'].get("products"))
+#         return tuple(names_)
+
+#     def statuses(self) -> tuple:
+#         """
+#         Gets the statuses of the products from the search results.
+#         :return: A tuple containing the statuses of the products.
+#         """
+#         statuses_ = (product.get("status") for page in range(1, self.pager() + 1)
+#                   for product in get(self.global_url+f"&page={page}").json()['data'].get("products"))
+#         return tuple(statuses_)
+    
+#     def urls(self) -> tuple:
+#         """
+#         Gets the urls of the products from the search results.
+#         :return: A tuple containing the urls of the products.
+#         """
+#         urls_ = ("www.digikala.com"+str(product['url'].get("uri")) for page in range(1, self.pager() + 1)
+#                   for product in get(self.global_url+f"&page={page}").json()['data'].get("products"))
+#         return tuple(urls_)   
+#     def prices(self) -> tuple:
+#         """
+#         Gets the prices of the products from the search results.
+#         :return: A tuple containing the prices of the products.
+#         """
+#         prices_ = ((product["default_variant"]["price"].get("selling_price")) for page in range(1,self.pager()+1 )\
+#                    for product in (get(self.global_url+f"&page={page}").json()['data']).get("products") if self.statuses()!="marketable" else 0)
+#         return tuple(prices_)
+
+#     # def average_stars(self):
+#     #     stars_ = (product["data_layer"].get("dimension9") for page in range(1, self.pager() + 1)
+#     #               for product in get(self.global_url+f"&page={page}").json()['data'].get("products"))
+
+#         # return tuple(stars_)
+#     def total_scores(self) -> tuple:
+#         """
+#         Gets the total scores of the products from the search results.
+#         :return: A tuple containing the total scores of the products.
+#         """
+#         total_scores_ = (product["rating"].get("rate") for page in range(1, self.pager() + 1)
+#                   for product in get(self.global_url+f"&page={page}").json()['data'].get("products"))
+
+#         return tuple(total_scores_)
+#     def count_of_scores(self) -> tuple:
+#         """
+#         Gets the number of scores for each product from the search results.
+#         :return: A tuple containing the number of scores for each product.
+#         """
+#         count_of_scores_ = (product["rating"].get("count") for page in range(1, self.pager() + 1)
+#                   for product in get(self.global_url+f"&page={page}").json()['data'].get("products"))
+
+#         return tuple(count_of_scores_)
+
+# dk=Digikala(input("?:"))
+# print(len(dk.prices()))
+
+
 from requests import get
 
 class Digikala:
-    def __init__(self, search_term:str):
+    def __init__(self, search_term: str):
         """
         Initializes a Digikala search object.
         :param search_term: The term to search in Digikala.
         """
         self.serch_term = search_term
         self.total_page_url = f"https://api.digikala.com/v1/search/?q={self.serch_term}&page=1"
-        self.global_url=f"https://api.digikala.com/v1/search/?q={self.serch_term}"
+        self.global_url = f"https://api.digikala.com/v1/search/?q={self.serch_term}"
 
     def pager(self) -> int:
         """
@@ -16,7 +99,7 @@ class Digikala:
         :return: The total number of pages in the search results.
         """
         total_pages = get(url=self.total_page_url).json()['data']['pager'].get("total_pages")
-        return total_pages
+        return int(total_pages)
 
     def names(self) -> tuple:
         """
@@ -44,15 +127,18 @@ class Digikala:
         urls_ = ("www.digikala.com"+str(product['url'].get("uri")) for page in range(1, self.pager() + 1)
                   for product in get(self.global_url+f"&page={page}").json()['data'].get("products"))
         return tuple(urls_)   
+    
+    # def prices(self) -> tuple:
+    #     """
+    #     Gets the prices of the products from the search results.
+    #     :return: A tuple containing the prices of the products.
+    #     """
+    #     prices_=(product["default_variant"]["price"].get("selling_price") for page in range(1,self.pager()+1) for product in (get(self.global_url+f"&page={page}").json()['data']).get("products") if product.get("status") == "marketable")
+    #     return tuple(prices_)
     def prices(self) -> tuple:
-        """
-        Gets the prices of the products from the search results.
-        :return: A tuple containing the prices of the products.
-        """
-        prices_ = ((product["default_variant"]["price"].get("selling_price")) for page in range(1, self.pager() + 1)
-                  for product in get(self.global_url+f"&page={page}").json()['data'].get("products"))
-        return tuple(prices_)
-           
+        return tuple(product["default_variant"]["price"].get("selling_price") if product.get("status") == "marketable" else 0 for page in range(1, self.pager()+1) for product in (get(self.global_url+f"&page={page}").json()['data']).get("products"))
+
+
     # def average_stars(self):
     #     stars_ = (product["data_layer"].get("dimension9") for page in range(1, self.pager() + 1)
     #               for product in get(self.global_url+f"&page={page}").json()['data'].get("products"))
@@ -67,6 +153,7 @@ class Digikala:
                   for product in get(self.global_url+f"&page={page}").json()['data'].get("products"))
 
         return tuple(total_scores_)
+    
     def count_of_scores(self) -> tuple:
         """
         Gets the number of scores for each product from the search results.
